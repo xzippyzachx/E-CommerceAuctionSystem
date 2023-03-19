7e34246b5dfe7101f5e37458d69bc4b6060a09f0
Zach <zacharyross1234@gmail.com>
2023-03-19 17:53:14 -0400

    Reverted accidental rename

--- DatabaseScripts/items/init.sql
+++ DatabaseScripts/items/init.sql
@@ -1,24 +1,24 @@
-CREATE DATABASE itemBeans;
+CREATE DATABASE items;
 
-\c itemBeans
+\c items
 
 CREATE OR REPLACE PROCEDURE item_data_reset()
 LANGUAGE plpgsql
 AS $$
 BEGIN
-	DELETE FROM itemBeans;
+	DELETE FROM items;
 	ALTER SEQUENCE itm_id_sequence RESTART WITH 1;
 
-	INSERT INTO itemBeans (itm_id, itm_name, itm_description , itm_shipping_cost)
+	INSERT INTO items (itm_id, itm_name, itm_description , itm_shipping_cost)
 	VALUES (nextval('itm_id_sequence'), 'Bike', 'Two wheel vehicle', 3);
 	
-	INSERT INTO itemBeans (itm_id, itm_name, itm_description , itm_shipping_cost)
+	INSERT INTO items (itm_id, itm_name, itm_description , itm_shipping_cost)
 	VALUES (nextval('itm_id_sequence'), 'Car', 'vehicle', 5);
 	
-	INSERT INTO itemBeans (itm_id, itm_name, itm_description , itm_shipping_cost)
+	INSERT INTO items (itm_id, itm_name, itm_description , itm_shipping_cost)
 	VALUES (nextval('itm_id_sequence'), 'Phone', 'Electornic Device', 4);
 	
-	INSERT INTO itemBeans (itm_id, itm_name, itm_description , itm_shipping_cost)
+	INSERT INTO items (itm_id, itm_name, itm_description , itm_shipping_cost)
 	VALUES (nextval('itm_id_sequence'), 'Laptop', 'i7-67000', 6);
 	
 END; $$;
