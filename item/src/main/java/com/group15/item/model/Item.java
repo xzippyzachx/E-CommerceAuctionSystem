d6c1ce862e5399ab21fdf054e91abc476a354901
mhassan04 <mahamedhassan052@gmail.com>
2023-03-21 00:08:21 -0400

    itm_expedited_cost

--- item/src/main/java/com/group15/item/model/Item.java
+++ item/src/main/java/com/group15/item/model/Item.java
@@ -23,14 +23,19 @@ public class Item {
 
     private Double itm_shipping_cost;
 
-    public Item(Integer itm_id, String itm_name, String itm_description, Double itm_shipping_cost) {
+    private Double itm_expedited_cost;
+
+    public Item(Integer itm_id, String itm_name, String itm_description, Double itm_shipping_cost, Double itm_expedited_cost) {
         this.itm_id = itm_id;
         this.itm_name = itm_name;
         this.itm_description = itm_description;
         this.itm_shipping_cost = itm_shipping_cost;
+        this.itm_expedited_cost = itm_expedited_cost;
     }
 
-    public Item(){}
+    public Item() {
+
+    }
 
     public Integer getItm_id() {
         return itm_id;
@@ -64,17 +69,25 @@ public class Item {
         this.itm_shipping_cost = itm_shipping_cost;
     }
 
+    public Double getItm_expedited_cost() {
+        return itm_expedited_cost;
+    }
+
+    public void setItm_expedited_cost(Double itm_expedited_cost) {
+        this.itm_expedited_cost = itm_expedited_cost;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Item item = (Item) o;
-        return Objects.equals(itm_id, item.itm_id) && Objects.equals(itm_name, item.itm_name) && Objects.equals(itm_description, item.itm_description) && Objects.equals(itm_shipping_cost, item.itm_shipping_cost);
+        return Objects.equals(itm_id, item.itm_id) && Objects.equals(itm_name, item.itm_name) && Objects.equals(itm_description, item.itm_description) && Objects.equals(itm_shipping_cost, item.itm_shipping_cost) && Objects.equals(itm_expedited_cost, item.itm_expedited_cost);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(itm_id, itm_name, itm_description, itm_shipping_cost);
+        return Objects.hash(itm_id, itm_name, itm_description, itm_shipping_cost, itm_expedited_cost);
     }
 
     @Override
@@ -84,6 +97,7 @@ public class Item {
                 ", itm_name='" + itm_name + '\'' +
                 ", itm_description='" + itm_description + '\'' +
                 ", itm_shipping_cost=" + itm_shipping_cost +
+                ", itm_expedited_cost=" + itm_expedited_cost +
                 '}';
     }
 }
